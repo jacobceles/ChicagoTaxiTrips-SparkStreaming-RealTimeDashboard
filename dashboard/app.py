21461c5809b5eb431887f263113f813d9dbc5dcc
Jacob Celestine <jacobceles@gmail.com>
2021-10-15 17:27:48 -0400

    Initial commit

---
+++ dashboard/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, jsonify, request
+from flask import render_template
+import ast
+
+app = Flask(__name__)
+
+labels0 = []
+values0 = []
+labels1 = []
+values1 = []
+labels2 = []
+values2_1 = []
+values2_2 = []
+values2_3 = []
+values2_4 = []
+labels3 = []
+values3 = []
+labels4 = []
+values4 = []
+labels5 = []
+values5 = []
+
+
+@app.route("/")
+def get_chart_page():
+    global labels0, values0, labels1, values1, labels2, values2_1, values2_2, values2_3, values2_4, \
+        labels3, values3, labels4, values4, labels5, values5
+    labels0 = []
+    values0 = []
+    labels1 = []
+    values1 = []
+    labels2 = []
+    values2_1 = []
+    values2_2 = []
+    values2_3 = []
+    values2_4 = []
+    labels3 = []
+    values3 = []
+    labels4 = []
+    values4 = []
+    labels5 = []
+    values5 = []
+    return render_template('index.html',
+                           labels0=labels0, values0=values0,
+                           labels1=labels1, values1=values1,
+                           labels2=labels2,
+                           values2_1=values2_1, values2_2=values2_2,
+                           values2_3=values2_3, values2_4=values2_4,
+                           labels3=labels3, values3=values3,
+                           labels4=labels4, values4=values4,
+                           labels5=labels5, values5=values5)
+
+
+@app.route('/refreshData')
+def refresh_graph_data():
+    global labels0, values0, labels1, values1, labels2, values2_1, values2_2, values2_3, values2_4, \
+        labels3, values3, labels4, values4, labels5, values5
+    print("Labels0 now: " + str(labels0))
+    print("Values0 now: " + str(values0))
+    print("Labels1 now: " + str(labels1))
+    print("Values1 now: " + str(values1))
+    print("Labels2 now: " + str(labels2))
+    print("Values2_1 now: " + str(values2_1))
+    print("Values2_2 now: " + str(values2_2))
+    print("Values2_3 now: " + str(values2_3))
+    print("Values2_4 now: " + str(values2_4))
+    print("Labels3 now: " + str(labels3))
+    print("Values3 now: " + str(values3))
+    print("Labels4 now: " + str(labels4))
+    print("Values4 now: " + str(values4))
+    print("Labels5 now: " + str(labels5))
+    print("Values5 now: " + str(values5))
+    return jsonify(labels0=labels0, values0=values0,
+                   labels1=labels1, values1=values1,
+                   labels2=labels2,
+                   values2_1=values2_1, values2_2=values2_2,
+                   values2_3=values2_3, values2_4=values2_4,
+                   labels3=labels3, values3=values3,
+                   labels4=labels4, values4=values4,
+                   labels5=labels5, values5=values5)
+
+
+@app.route('/updateData', methods=['POST'])
+def update_data():
+    global labels0, values0, labels1, values1, labels2, values2_1, values2_2, values2_3, values2_4, \
+        labels3, values3, labels4, values4, labels5, values5
+    if 'values0' in str(request.form):
+        labels0 = ast.literal_eval(request.form['labels0'])
+        values0 = ast.literal_eval(request.form['values0'])
+    if 'values1' in str(request.form):
+        labels1 = ast.literal_eval(request.form['labels1'])
+        values1 = ast.literal_eval(request.form['values1'])
+    elif 'values2_1' in str(request.form):
+        labels2 = ast.literal_eval(request.form['labels2'])
+        values2_1 = ast.literal_eval(request.form['values2_1'])
+    elif 'values2_2' in str(request.form):
+        labels2 = ast.literal_eval(request.form['labels2'])
+        values2_2 = ast.literal_eval(request.form['values2_2'])
+    elif 'values2_3' in str(request.form):
+        labels2 = ast.literal_eval(request.form['labels2'])
+        values2_3 = ast.literal_eval(request.form['values2_3'])
+    elif 'values2_4' in str(request.form):
+        labels2 = ast.literal_eval(request.form['labels2'])
+        values2_4 = ast.literal_eval(request.form['values2_4'])
+    elif 'values3' in str(request.form):
+        labels3 = ast.literal_eval(request.form['labels3'])
+        values3 = ast.literal_eval(request.form['values3'])
+    elif 'values4' in str(request.form):
+        labels4 = ast.literal_eval(request.form['labels4'])
+        values4 = ast.literal_eval(request.form['values4'])
+    elif 'values5' in str(request.form):
+        labels5 = ast.literal_eval(request.form['labels5'])
+        values5 = ast.literal_eval(request.form['values5'])
+    else:
+        return "error", 400
+    print("Labels0 Received: " + str(labels0))
+    print("Values0 Received: " + str(values0))
+    print("Labels1 Received: " + str(labels1))
+    print("Values1 Received: " + str(values1))
+    print("Labels2 Received: " + str(labels2))
+    print("Values2_1 Received: " + str(values2_1))
+    print("Values2_2 Received: " + str(values2_2))
+    print("Values2_3 Received: " + str(values2_3))
+    print("Values2_4 Received: " + str(values2_4))
+    print("Labels3 Received: " + str(labels3))
+    print("Values3 Received: " + str(values3))
+    print("Labels4 Received: " + str(labels4))
+    print("Values4 Received: " + str(values4))
+    print("Labels5 Received: " + str(labels5))
+    print("Values5 Received: " + str(values5))
+    return "success", 201
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5001)
---
+++ streaming.py
@@ -0,0 +1,148 @@
+"""
+Questions:
+1. Rate of tipping over years?
+2. Popular taxi trips days:
+    Number of records vs Year
+3. Mode of payment over years?
+4. Total miles travelled across years?
+5. Total time travelled across years?
+6. Which company makes the most trips per year?
+"""
+import requests
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, concat, lit, to_timestamp, year, month, dayofmonth, to_date
+
+
+def define_schema():
+    labels = [('Trip ID', StringType()),
+              ('Taxi ID', StringType()),
+              ('Trip Start Timestamp', StringType()),
+              ('Trip End Timestamp', StringType()),
+              ('Trip Seconds', DoubleType()),
+              ('Trip Miles', DoubleType()),
+              ('Pickup Census Tract', DoubleType()),
+              ('Dropoff Census Tract', DoubleType()),
+              ('Pickup Community Area', DoubleType()),
+              ('Dropoff Community Area', DoubleType()),
+              ('Fare', DoubleType()),
+              ('Tips', DoubleType()),
+              ('Tolls', DoubleType()),
+              ('Extras', DoubleType()),
+              ('Trip Total', DoubleType()),
+              ('Payment Type', StringType()),
+              ('Company', StringType()),
+              ('Pickup Centroid Latitude', StringType()),
+              ('Pickup Centroid Longitude', StringType()),
+              ('Pickup Centroid Location', StringType()),
+              ('Dropoff Centroid Latitude', StringType()),
+              ('Dropoff Centroid Longitude', StringType()),
+              ('Dropoff Centroid  Location', StringType(),
+              ('Month', IntegerType()))]
+    return StructType([StructField(x[0], x[1], True) for x in labels])
+
+
+def foreach_batch_function(df, epoch_id):
+    request_data = {}
+    if 'total_tips' in df.columns:
+        labels = [str(t.year) for t in df.select("year").collect()]
+        data = [p.total_tips for p in df.select("total_tips").collect()]
+        request_data = {'labels0': str(labels), 'values0': str(data)}
+    elif 'total_taxi_trip' in df.columns:
+        labels = [str(t.date) for t in df.select("date").collect()]
+        data = [p.total_taxi_trip for p in df.select("total_taxi_trip").collect()]
+        request_data = {'labels1': str(labels), 'values1': str(data)}
+    elif 'count_per_category' in df.columns:
+        payment_type = df.head()[0].lower()
+        labels = [str(t.year) for t in df.select("year").collect()]
+        data = [p.count_per_category for p in df.select("count_per_category").collect()]
+        if payment_type == "cash":
+            request_data = {'labels2': str(labels), 'values2_1': str(data)}
+        elif payment_type == "mobile":
+            request_data = {'labels2': str(labels), 'values2_2': str(data)}
+        elif payment_type == "prcard":
+            request_data = {'labels2': str(labels), 'values2_3': str(data)}
+        elif payment_type == "credit card":
+            request_data = {'labels2': str(labels), 'values2_4': str(data)}
+        else:
+            print("Invalid type: " + str(payment_type))
+    elif 'total_trip_miles' in df.columns:
+        labels = [str(t.year) for t in df.select("year").collect()]
+        data = [p.total_trip_miles for p in df.select("total_trip_miles").collect()]
+        request_data = {'labels3': str(labels), 'values3': str(data)}
+    elif 'total_trip_hours' in df.columns:
+        labels = [str(t.year) for t in df.select("year").collect()]
+        data = [p.total_trip_hours for p in df.select("total_trip_hours").collect()]
+        request_data = {'labels4': str(labels), 'values4': str(data)}
+    elif 'total_rides' in df.columns:
+        df_temp1 = df.groupBy('year').max("total_rides").select("max(total_rides)")\
+            .withColumnRenamed("max(total_rides)", "total_rides_count")
+        df_temp2 = df.join(df_temp1, df.total_rides == df_temp1.total_rides_count, "inner")\
+            .select(concat(col("year"), lit(": "), col("company")).alias("name"), "total_rides")
+        labels = [str(t.name) for t in df_temp2.select("name").collect()]
+        data = [p.total_rides for p in df_temp2.select("total_rides").collect()]
+        request_data = {'labels5': str(labels), 'values5': str(data)}
+    # initialize and send the data through REST API
+    url = 'http://localhost:5001/updateData'
+    print(request_data)
+    requests.post(url, data=request_data)
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Chicago Taxi Trip Streaming Analysis").getOrCreate()
+    spark.sparkContext.setLogLevel("FATAL")
+
+    schema = define_schema()
+    dataframe = spark.readStream.format("csv").schema(schema)\
+        .option("header", True).option("cleanSource", "off")\
+        .option("ignoreLeadingWhiteSpace", True).option("mode", "dropMalformed")\
+        .option("maxFilesPerTrigger", 1).load("source")
+
+    # Cleaning
+    dataframe = dataframe.toDF(*[c.lower().replace(" ", "_") for c in dataframe.columns]) \
+        .withColumn('trip_start_timestamp', to_timestamp(col('trip_start_timestamp'), 'MM/dd/yyyy hh:mm:ss a')) \
+        .withColumn('trip_end_timestamp', to_timestamp(col('trip_end_timestamp'), 'MM/dd/yyyy hh:mm:ss a')) \
+        .withColumn('date', to_date(col('trip_start_timestamp')))\
+        .withColumn('year', year(col('trip_start_timestamp')))\
+        .withColumn('month', month(col('trip_start_timestamp')))\
+        .withColumn('day', dayofmonth(col('trip_start_timestamp')))
+
+    # Create a view
+    dataframe.createOrReplaceTempView("taxi_trips")
+
+    # 1. Rate of tipping over years?
+    spark.sql("""SELECT year, SUM(tips) AS total_tips FROM taxi_trips GROUP BY year""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # 2. Popular taxi trips days: Number of records vs Year
+    spark.sql("""SELECT date, COUNT(trip_id) AS total_taxi_trip FROM taxi_trips GROUP BY date""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # 3. Mode of payment over year
+    spark.sql("""SELECT payment_type, year, COUNT(trip_id) AS count_per_category FROM taxi_trips
+    GROUP BY payment_type, year HAVING lower(payment_type)=='cash'""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+    spark.sql("""SELECT payment_type, year, COUNT(trip_id) AS count_per_category FROM taxi_trips
+    GROUP BY payment_type, year HAVING lower(payment_type)=='mobile'""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+    spark.sql("""SELECT payment_type, year, COUNT(trip_id) AS count_per_category FROM taxi_trips
+    GROUP BY payment_type, year HAVING lower(payment_type)=='prcard'""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+    spark.sql("""SELECT payment_type, year, COUNT(trip_id) AS count_per_category FROM taxi_trips
+    GROUP BY payment_type, year HAVING lower(payment_type)=='credit card'""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # 4. Total miles taxis travelled across years?
+    spark.sql("""SELECT year, SUM(trip_miles) AS total_trip_miles FROM taxi_trips GROUP BY year""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # 5. Total time travelled across years?
+    spark.sql("""SELECT year, SUM(trip_seconds)/3600 AS total_trip_hours FROM taxi_trips GROUP BY year""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # 6. Which company makes the most trips per year?
+    spark.sql("""SELECT year, company, COUNT(*) as total_rides FROM taxi_trips GROUP BY year, company""")\
+        .writeStream.outputMode("complete").foreachBatch(foreach_batch_function).start()
+
+    # Stop only when I say so
+    spark.streams.awaitAnyTermination()
